5bfb2bf77db4694462d36d940ac3cbfe388daf92
LorenaGomes <lorena@carzen.com.br>
2021-12-28 22:59:59 -0300

    trying to fix encoding

--- src/main/java/com/logcy/shorturl/ShortUrlApplication.java
+++ src/main/java/com/logcy/shorturl/ShortUrlApplication.java
@@ -2,12 +2,23 @@ package com.logcy.shorturl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
 
 @SpringBootApplication
-public class ShortUrlApplication {
+public class ShortUrlApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
+		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
 		SpringApplication.run(ShortUrlApplication.class, args);
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		UrlPathHelper urlPathHelper = new UrlPathHelper();
+		urlPathHelper.setUrlDecode(false);
+		configurer.setUrlPathHelper(urlPathHelper);
 	}
 
 }
